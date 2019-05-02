67ec37e9c81c2b9ced641af69432276a7d6d723e
K <774162+kaseea@users.noreply.github.com>
2019-05-01 19:15:21 -0700

    odds migrations without spelling mistakes

--- db/migrate/20190502015814_adds_all_foreign_keys.rb
+++ db/migrate/20190502015814_adds_all_foreign_keys.rb
@@ -1,7 +1,7 @@
 class AddsAllForeignKeys < ActiveRecord::Migration[5.2]
   def change
-    add_refrence :products, :user, foreign_key: true
-    add_refrence :orderitems, :product, foreign_key: true
-    add_refrence :orderitems, :order, foreign_key: true
+    add_reference :products, :user, foreign_key: true
+    add_reference :order_items, :product, foreign_key: true
+    add_reference :order_items, :order, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_221440) do
+ActiveRecord::Schema.define(version: 2019_05_02_015814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 2019_05_01_221440) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_id"
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -52,6 +56,10 @@ ActiveRecord::Schema.define(version: 2019_05_01_221440) do
     t.integer "inventory"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_url"
+    t.string "description"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -66,4 +74,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_221440) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "products", "users"
 end
