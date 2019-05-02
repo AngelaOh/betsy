e854ba4b82208e5de7fd77f8cb16005d21f423ae
Angela <angela.ohh@gmail.com>
2019-05-01 21:07:36 -0700

    added index user action and tests for index & show action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,34 @@
 require "test_helper"
 
 describe UsersController do
-  it "should get index" do
-    get users_index_url
-    value(response).must_be :success?
+  let(:user) { users(:one) }
+  let(:invalid_id) { "INVALID ID" }
+  describe "index" do
+    it "should get index when users exist" do
+      get users_path
+
+      must_respond_with :success
+    end
+    it "should get index when no users exist" do
+      User.all do |user|
+        user.destroy
+      end
+
+      get users_path
+
+      must_respond_with :success
+    end
   end
 
-  it "should get show" do
-    get users_show_url
-    value(response).must_be :success?
-  end
+  describe "show" do
+    it "should get respond with success if user exists" do
+      get user_path(user.id)
+      must_respond_with :success
+    end
 
+    it "should get respond with 404 not found if ID is invalid" do
+      get user_path(invalid_id)
+      must_respond_with :not_found
+    end
+  end
 end
