d0753a41b4c4db854157b59c25b9923047a2bd7a
Angela <angela.ohh@gmail.com>
2019-05-01 21:29:11 -0700

    added user show action and test view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show]
+
   def index
     @users = User.all
   end
 
   def show
+    if @user.nil?
+      flash[:status] = :failure
+      flash[:result_text] = "User not found!"
+      flash[:messages] = @user.errors.messages
+      redirect_to root_path # AO: Where should we redirect this?
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(id: params[:id])
   end
 end
