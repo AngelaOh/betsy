9afa0ead136e199319a8abfa483e87cd0aa11ff2
K <774162+kaseea@users.noreply.github.com>
2019-05-02 15:22:21 -0700

    changes orderitems to order_items on model relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ApplicationRecord
   has_and_belongs_to_many :categories
   belongs_to :user
   #we are required to validate that product must belong to a user, cause looks likes belongs_to requires that after rails 5
-  has_many :orders, through: :orderitems
-  has_many :orderitems
+  has_many :orders, through: :order_items
+  has_many :order_items
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: {only_integer: true, greater_than: 0}
