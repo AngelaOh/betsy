78994919f51f455cf980d499a8b55ef33c15ec21
Angela <angela.ohh@gmail.com>
2019-05-04 11:51:02 -0700

    added ability to change order status and destroy items once checked out

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,16 @@
 class OrdersController < ApplicationController
-  def cart
+  def cart # One cart (@order) holds information on zero or many OrderItems
     @order = Order.new(status: "pending")
     @items = OrderItem.all
   end
 
-  def show
+  def new_order_item
+    # TODO: Quantity should come from product#show view; hardcoded for now
+    @item = OrderItem.new(quantity: 1, order_id: @order.id, product_id: params[:id])
   end
 
-  def new_order_item
-    # TODO: Quantity should come from product show view; hardcoded for now
-    @item = OrderItem.new(quantity: 1, order_id: @order.id, product_id: params[:id])
+  def show # once user clicks checkout from order#cart view, the status should change to the next one. What are our statuses?
+    @order.status = "shipping"
+    @items.destroy
   end
 end
