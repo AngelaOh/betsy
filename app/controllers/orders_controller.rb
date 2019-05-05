2d812d1b86f83eecfe95394123e2bcfd5e3b53c2
Angela <angela.ohh@gmail.com>
2019-05-05 14:39:09 -0700

    added logic in order controller to account for @order and/or @items as nil

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,19 @@
 class OrdersController < ApplicationController
   def cart # One cart (@order) holds information on zero or many OrderItems
     @order = Order.find_by(status: "pending") # under the assumption there is only one order happening at a time, does this logic work?
-    @items = OrderItem.where(order_id: @order.id)
+    if @order && order.order_items.length != 0
+      @items = OrderItem.where(order_id: @order.id)
+    end
   end
 
   def new_order_item
     # TODO: Quantity should come from product#show view; hardcoded for now
-    @order = Order.create(status: "pending")
+    if Order.find_by(status: "pending")
+      @order = Order.find_by(status: "pending")
+    else
+      @order = Order.create(status: "pending")
+    end
+
     @item = OrderItem.create(quantity: 1, order_id: @order.id, product_id: params[:id])
     @order.order_items << @item
     redirect_to product_path(params[:id])
