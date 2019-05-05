2d7d22fc1f2bf6b42a7bfd53a4283fc8080706ff
Angela <angela.ohh@gmail.com>
2019-05-05 14:23:14 -0700

    changed @items in products#card so we access teh right orderitems

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,13 @@
 class OrdersController < ApplicationController
   def cart # One cart (@order) holds information on zero or many OrderItems
-    @order = Order.find_by(status: "pending")
-    @items = OrderItem.all
+    @order = Order.find_by(status: "pending") # under the assumption there is only one order happening at a time, does this logic work?
+    @items = OrderItem.where(order_id: @order.id)
   end
 
   def new_order_item
     # TODO: Quantity should come from product#show view; hardcoded for now
     @order = Order.create(status: "pending")
     @item = OrderItem.create(quantity: 1, order_id: @order.id, product_id: params[:id])
-    # @item.save
-    # raise
     @order.order_items << @item
     redirect_to product_path(params[:id])
   end
@@ -21,7 +19,7 @@ class OrdersController < ApplicationController
   end
 
   def show # once user clicks checkout from order#cart view, the status should change to the next one. What are our statuses?
-    @order.status = "shipping"
+    @order.status = "paid"
     Products.find_by(@item.product_id).inventory = Products.find_by(@item.product_id).inventory - @items.quantity #change inventory of Products
     @items.destroy # I want to destroy associated OrderItems
     @order.destroy
