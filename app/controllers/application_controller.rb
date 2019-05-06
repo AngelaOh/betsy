c650e8625e631d6b088bfaee404bde6fa402f5ae
Shamira <marshallscm@gmail.com>
2019-05-06 16:55:44 -0700

    Application Controller Filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def require_login
+    @user = User.find_by(id: session[:user_id])
+    if @user.nil?
+      flash[:error] = "You must be logged in to view this page!"
+      redirect_to root_path
+    end
+  end
 end
