b30ac6788e505d01b5b976e3349f7d7f5eaa0f4f
Angela <angela.ohh@gmail.com>
2019-05-07 15:48:53 -0700

    added validation for unique product and order id per order item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,5 +3,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
 
   #maybe add a unique combindation requirement of order id and product id
-  validates :quantity, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :order_id, uniqueness: { scope: :product_id }
 end
