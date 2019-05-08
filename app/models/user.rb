377e47544cf1a120c463635ee85324128a2dc001
K <774162+kaseea@users.noreply.github.com>
2019-05-07 17:09:23 -0700

    changes github login from user to nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   def self.build_from_github(auth_hash)
     return User.new(uid: auth_hash[:uid], provider: "github",
                     email: auth_hash["info"]["email"],
-                    username: auth_hash["info"]["name"])
+                    username: auth_hash["info"]["nickname"])
   end
 
   def findorder
