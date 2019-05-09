11a3729f354b74aeb63f8132096892dc818c7af8
Shamira <marshallscm@gmail.com>
2019-05-09 09:04:02 -0700

    passing categories test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     if @merchant.nil?
       flash[:error] = "You must be logged in to view this page!"
       redirect_to root_path
-    elsif params[:id] != @merchant.id.to_s
+    elsif (1 <= params[:id].to_i) && (params[:id].to_i <= User.all.length) && params[:id] != @merchant.id.to_s
       flash[:error] = "Must be this merchant to view page!"
       redirect_to root_path
     end
