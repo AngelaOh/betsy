3b6b780ab0cbc0954dbfced046eb9ba17db2020e
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-09 19:26:43 -0700

    fixed incorrect lets for yml fixtures

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 describe OrdersController do
   let(:order) { orders(:first) }
-  let(:paid_order) { orders(:three) }
-  let(:cancelled_order) { orders(:four) }
+  let(:paid_order) { orders(:four) }
+  let(:cancelled_order) { orders(:five) }
   let(:shipped_order) { orders(:two) }
   let(:order_item1) { order_items(:one) }
   let(:order_item2) { order_items(:two) }
@@ -33,7 +33,7 @@ describe OrdersController do
       expect(empty_order).must_equal nil
 
       expect {
-        post add_item_path(product.id), params: { quantity: 1 }
+        post add_item_path(product.id), params: {quantity: 1}
       }.must_change "OrderItem.count", 1
 
       new_order = Order.find_by(id: session[:order_id])
@@ -47,7 +47,7 @@ describe OrdersController do
       product_inventory = product.inventory
 
       expect {
-        post add_item_path(product.id), params: { quantity: 1 }
+        post add_item_path(product.id), params: {quantity: 1}
       }.must_change "OrderItem.count", 1
       product.reload
 
@@ -59,27 +59,27 @@ describe OrdersController do
 
     it "updates the inventory for correct product when an existing orderitem is added to (via product#show)" do
       product_inventory = product.inventory
-      post add_item_path(product.id), params: { quantity: 1 }
+      post add_item_path(product.id), params: {quantity: 1}
       product.reload
       expect(product.inventory).must_equal product_inventory - 1
 
-      post add_item_path(product.id), params: { quantity: 1 }
+      post add_item_path(product.id), params: {quantity: 1}
       product.reload
       expect(product.inventory).must_equal product_inventory - 2
     end
 
     it "updates the quantity for correct orderitem when an existing orderitem is added to (via product#show)" do
-      post add_item_path(product.id), params: { quantity: 1 }
+      post add_item_path(product.id), params: {quantity: 1}
       new_order = Order.find_by(id: session[:order_id])
       new_order_item = new_order.order_items[0]
 
-      post add_item_path(product.id), params: { quantity: 1 }
+      post add_item_path(product.id), params: {quantity: 1}
       new_order_item.reload
       expect(new_order_item.quantity).must_equal 2
     end
 
     it "flashes an error and redirects if the the quantity to update is larger than the available inventory" do
-      post add_item_path(product.id), params: { quantity: 70 }
+      post add_item_path(product.id), params: {quantity: 70}
       expect(flash[:error]).must_equal "We don't have enough items in inventory to fulfill this order."
       must_respond_with :redirect
     end
@@ -98,28 +98,28 @@ describe OrdersController do
   describe "update order item quantity" do
     it "updates product inventory" do
       product_inventory = product.inventory
-      post add_item_path(product.id), params: { quantity: 1 }
+      post add_item_path(product.id), params: {quantity: 1}
       product.reload
       expect(product.inventory).must_equal product_inventory - 1
 
-      patch update_quantity_path(product.id), params: { order_item: { quantity: 3 } }
+      patch update_quantity_path(product.id), params: {order_item: {quantity: 3}}
       product.reload
       expect(product.inventory).must_equal product_inventory - 3
     end
 
     it "updates orderitem quantity" do
-      post add_item_path(product.id), params: { quantity: 1 }
+      post add_item_path(product.id), params: {quantity: 1}
       order_item = OrderItem.find_by(order_id: session[:order_id])
       order_item.reload
 
-      patch update_quantity_path(product.id), params: { order_item: { quantity: 3 } }
+      patch update_quantity_path(product.id), params: {order_item: {quantity: 3}}
       expect(OrderItem.find_by(order_id: session[:order_id]).quantity).must_equal 3
     end
 
     it "flashes error and redirects if the desired quantity is more than the inventory available" do
-      post add_item_path(product.id), params: { quantity: 1 }
+      post add_item_path(product.id), params: {quantity: 1}
 
-      patch update_quantity_path(product.id), params: { order_item: { quantity: 100000 } }
+      patch update_quantity_path(product.id), params: {order_item: {quantity: 100000}}
       expect(flash[:error]).must_equal "We don't have enough items in inventory to fulfill this order."
     end
   end
@@ -127,11 +127,11 @@ describe OrdersController do
   describe "update" do
     it "updates an order with the checkout information" do
       # patch "/orders/:id", to: "orders#update", as: "order_update"
-      post add_item_path(product.id), params: { quantity: 1 }
+      post add_item_path(product.id), params: {quantity: 1}
       new_order = Order.find_by(id: session[:order_id])
       expect(new_order.order_items.length).must_equal 1
 
-      patch order_update_path(new_order.id), params: { order: { status: "pending", name: "blah", email: "blah", address: "blah", credit_card: 999, exp: 999 } }
+      patch order_update_path(new_order.id), params: {order: {status: "pending", name: "blah", email: "blah", address: "blah", credit_card: 999, exp: 999}}
       new_order.reload
 
       must_respond_with :redirect
@@ -142,18 +142,18 @@ describe OrdersController do
       order = Order.create(status: "pending")
       order_id = Order.create(status: "pending").id
       order.destroy
-      patch order_update_path(order_id), params: { order: { status: "pending", name: "blah", email: "blah", address: "blah", credit_card: 999, exp: 999 } }
+      patch order_update_path(order_id), params: {order: {status: "pending", name: "blah", email: "blah", address: "blah", credit_card: 999, exp: 999}}
 
       expect(flash[:error]).must_equal "This order does not exist"
       must_respond_with :redirect
     end
 
     it "responds with bad request if data is not valid" do
-      post add_item_path(product.id), params: { quantity: 1 }
+      post add_item_path(product.id), params: {quantity: 1}
       new_order = Order.find_by(id: session[:order_id])
       expect(new_order.order_items.length).must_equal 1
 
-      patch order_update_path(new_order.id), params: { order: { status: "pending", name: "", email: "blah", address: "blah", credit_card: 999, exp: 999 } }
+      patch order_update_path(new_order.id), params: {order: {status: "pending", name: "", email: "blah", address: "blah", credit_card: 999, exp: 999}}
       new_order.reload
 
       must_respond_with :bad_request
@@ -193,7 +193,7 @@ describe OrdersController do
 
   describe "show" do
     it "sets current session[:order_id] to be nil" do
-      post add_item_path(product.id), params: { quantity: 1 }
+      post add_item_path(product.id), params: {quantity: 1}
       new_order = Order.find_by(id: session[:order_id])
       expect(session[:order_id]).wont_be_nil
 
