bf9a9f2e8db6cef92e521b8142bcd2b451fad64a
Angela <angela.ohh@gmail.com>
2019-05-10 10:38:21 -0700

    got rid of commented out lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    # raise
     if !@user
       flash[:status] = :failure
       flash[:result_text] = "User not found!"
       redirect_to root_path
     end
-    # @user_orders = @user.findorder
   end
 
   def login
@@ -33,7 +31,6 @@ class UsersController < ApplicationController
         flash[:result_text] = "Could not create new user account."
         return redirect_to root_path
       end
-      #redirct to homepage
     end
 
     session[:user_id] = user.id
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,10 @@
 class Product < ApplicationRecord
   has_and_belongs_to_many :categories
-  #should it be has many categories through that one table?
   belongs_to :user
-  #we are required to validate that product must belong to a user, cause looks likes belongs_to requires that after rails 5
   has_many :orders, through: :order_items
   has_many :order_items
 
   validates :name, presence: true, uniqueness: true
-  validates :price, :inventory, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :price, :inventory, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :description, :photo_url, presence: true
-  # validates :categories, presence: true
-  # next time when we add categories table
 end
